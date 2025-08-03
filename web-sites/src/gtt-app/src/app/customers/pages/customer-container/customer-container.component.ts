abf216ff4ffd821dd698e67fd04fd073a9802462
Alejandro Junqui <ajunquit@gmail.com>
2025-08-02 20:27:26 -0500

    add customer service and subscriber into container

--- web-sites/src/gtt-app/src/app/customers/pages/customer-container/customer-container.component.ts
+++ web-sites/src/gtt-app/src/app/customers/pages/customer-container/customer-container.component.ts
@@ -1,19 +1,28 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ModalComponent } from '../../../shared/components/modal/modal.component';
+import { ModalAction } from '../../../shared/enums/modal-action.enum';
 import { CustomerFormComponent } from '../../components/customer-form/customer-form.component';
 import { CustomerListComponent } from '../../components/customer-list/customer-list.component';
 import { Customer } from '../../models/customer.model';
-import { ModalAction } from '../../../shared/enums/modal-action.enum';
+import { CustomerService } from '../../services/customer.service';
 
 @Component({
   selector: 'app-customer-container',
   standalone: true,
-  imports: [CustomerListComponent, CustomerFormComponent, ModalComponent],
+  imports: [
+    CustomerListComponent,
+    CustomerFormComponent,
+    ModalComponent,
+    HttpClientModule,
+  ],
   templateUrl: './customer-container.component.html',
   styleUrl: './customer-container.component.scss',
 })
 export class CustomerContainerComponent implements OnInit {
   public customers: Customer[] = [];
+
+  constructor(private customerService: CustomerService) {}
 
   ngOnInit(): void {
     this.internalinit();
@@ -75,20 +84,37 @@ export class CustomerContainerComponent implements OnInit {
   }
 
   public handleSaveAction(customer: Customer) {
-    if (customer) {
-      // call api to save new customer
-      this.showModal(false);
-      this.currentCustomer = this.emptyCustomer();
+    if (customer && this.isNewCustomer) {
+      this.customerService.create(customer).subscribe({
+        next: () => {
+          this.loadCustomers();
+          this.showModal(false);
+          this.currentCustomer = this.emptyCustomer();
+        },
+        error: (err) => console.error('Error al crear cliente ', err),
+      });
     }
   }
 
   public handleEditAction(customer: Customer) {
-    if (customer) {
-      // call api to edit new customer
-      this.isNewCustomer = false;
-      this.currentCustomer = customer;
-      this.showModal(true);
+    if (customer && customer.id && !this.isNewCustomer) {
+      this.customerService.update(customer.id, customer).subscribe({
+        next: () => {
+          this.loadCustomers();
+          this.isNewCustomer = false;
+          this.currentCustomer = customer;
+          this.showModal(true);
+        },
+        error: (err) => console.error('Error al editar ', err),
+      });
     }
+  }
+
+  private loadCustomers() {
+    this.customerService.getAll().subscribe({
+      next: (data) => (this.customers = data),
+      error: (err) => console.error('Error al cargar clientes', err),
+    });
   }
 
   private emptyCustomer(): Customer {
---
+++ web-sites/src/gtt-app/src/app/customers/services/customer.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Customer } from '../models/customer.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CustomerService {
+  private readonly baseUrl = '';
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Customer[]> {
+    return this.http.get<Customer[]>(this.baseUrl);
+  }
+
+  getById(id: string): Observable<Customer> {
+    return this.http.get<Customer>(`${this.baseUrl}/${id}`);
+  }
+
+  create(customer: Customer): Observable<Customer> {
+    return this.http.post<Customer>(this.baseUrl, customer);
+  }
+
+  update(id: string, customer: Customer): Observable<Customer> {
+    return this.http.put<Customer>(`${this.baseUrl}/${id}`, customer);
+  }
+
+  delete(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+  }
+}
