c4c537da2af58003ebe32f1661f83e808c4bd8cf
Alejandro Junqui <ajunquit@gmail.com>
2025-08-05 21:30:58 -0500

    add infrastructures

---
+++ web-apis/business-service/src/Business.Service.Infrastructure/Persistence/Configurations/OrderConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Business.Service.Domain.Orders.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Business.Service.Infrastructure.Persistence.Configurations
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.Property(o => o.TotalAmount)
+                   .HasPrecision(18, 2);
+
+            builder.HasOne(o => o.Customer)
+                   .WithMany()
+                   .HasForeignKey(o => o.CustomerId)
+                   .IsRequired();
+        }
+    }
+}
---
+++ web-apis/business-service/src/Business.Service.Infrastructure/Persistence/Contexts/ModelBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Business.Service.Infrastructure.Persistence.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Business.Service.Infrastructure.Persistence.Contexts
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void ApplyEntityConfigurations(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new OrderConfiguration());
+        }
+
+    }
+}
