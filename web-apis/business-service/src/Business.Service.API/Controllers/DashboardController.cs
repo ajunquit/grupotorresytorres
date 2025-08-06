6bd98e2fc3989e66f7b3688193efaea23a1f8e6b
Alejandro Junqui <ajunquit@gmail.com>
2025-08-05 21:34:00 -0500

    add controllers

---
+++ web-apis/business-service/src/Business.Service.API/Controllers/DashboardController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Business.Service.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashboardController : ControllerBase
+    {
+    }
+}
